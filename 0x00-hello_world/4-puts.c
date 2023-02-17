198f65068d169a3333e5abf58cc83f0ae4096b6a
skitomie <skitomie@gmail.com>
2023-02-16 19:34:02 -0800

    initial commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - initialization
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+puts(Mystr);
+
+return (0);
+}
