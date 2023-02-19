3f7138d3c7659cc294dc7d9c2451702d5899a132
skitomie <skitomie@gmail.com>
2023-02-19 09:00:18 -0800

    initial commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
-* main - prints the alphabet.
-*
-* Return: Always 0 (Success)
-*/
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-int i;
-for (i = 0; i < 26; i++)
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
 {
-putchar(alp[i]);
+putchar(ch);
 }
-putchar (‘\n' );
+putchar('\n');
 return (0);
 }
+
