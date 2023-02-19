882b622c308cc3aceb92e555abd41928b03d9faf
skitomie <skitomie@gmail.com>
2023-02-19 09:10:50 -0800

    initial commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase, and then in uppercase,
- *
- * followed by a new line
- *
- * Return: Always 0 (Success)
+* main - prints the alphabet in lowercase, and then in uppercase,
+*
+* followed by a new line
+*
+* Return: Always 0 (Success)
 */
 int main(void)
 {
